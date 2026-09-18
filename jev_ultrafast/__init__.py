794349f810f57a2a612f80be3d3b7bbd89551ab6
Muresda <67793631+Muredsa@users.noreply.github.com>
2026-09-19 00:52:43 +0900

    Enable OpenRouter adapter on package import

--- jev_ultrafast/__init__.py
+++ jev_ultrafast/__init__.py
@@ -1,5 +1,9 @@
 """Jev chooses an observed action. Code owns execution."""
 
+from .openrouter import configure as _configure_openrouter
+
+_configure_openrouter()
+
 from .agent import Agent
 from .browser import Browser
 
